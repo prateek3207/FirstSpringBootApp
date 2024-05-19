11b70223b38b61a4a58ca9be555a65126f38130e
Prateek Sharma <prateeksharma@Prateeks-MacBook-Air.local>
2024-05-19 10:14:56 +0530

    First commit

---
+++ src/main/java/com/prateek/FirstSpringBootApp/api/UserController.java
@@ -0,0 +1,13 @@
+package com.prateek.FirstSpringBootApp.api;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/user/{userID}")
+    public String getUserData(@PathVariable("userID") String userID,
+                              @RequestParam("age") String age) {
+        return "Prateek sharma having user id " + userID + " and age: " + age ;
+    }
+}
